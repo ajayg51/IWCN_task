4a880571efe2c3bde15066ddae6abb5b2c79583b
ajayg51 <ajayg.ug18.cs@nitp.ac.in>
2024-04-28 15:25:14 +0530

    task 2 boilerplate code ready

--- task2/node_server.js
+++ task2/node_server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mysql = require('mysql');
+const bodyParser = require('body-parser')
 const cors = require('cors');
-
 
 const app = express();
 app.use(cors())
+
+const jsonParser = bodyParser.json()
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -26,15 +28,49 @@ app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
 
-app.get("/", (req, res) => {
+app.get("/get-notes", (req, res) => {
     console.log("Home page");
-    res.writeHead(200, { "Content-type": "text/html" })
-    res.write("Will get data from db");
-    res.end();
+    let rowList = [];
+
+    const query = `SELECT * FROM Notes`
+    connection.query(query, (err, resultSet, fields) => {
+        if (err) {
+            console.log("Error while select * from table", err)
+            return;
+        }
+        console.log(resultSet);
+
+        rowList = resultSet.map(item => {
+            return {
+                id: item.id,
+                note_title: item.note_title,
+                note_desc: item.note_desc
+            }
+        });
+
+        console.log(rowList)
+
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.send(rowList)
+    });
+
+
 })
 
-app.post("/post-data", (req, res) => {
-    console.log("post")
+app.post("/post-note", jsonParser, (req, res) => {
+    console.log("post-api called")
     console.log(req.body);
+    const noteId = 0;
+    const noteTitle = req.body["note_title"]
+    const noteDesc = req.body["note_desc"]
+    console.log(noteTitle);
+    console.log(noteDesc);
+
+    const query = `INSERT INTO Notes(id,note_title,note_desc) VALUES(0,"${noteTitle}","${noteDesc}")`
+    connection.query(query);
+    res.send({
+        msg: "Successfully added data in db"
+    });
 })
 
